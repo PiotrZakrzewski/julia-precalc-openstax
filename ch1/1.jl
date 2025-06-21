dc99e08dd15723f58b97d47bb5f0199bbb34c957
Piotr Zakrzewski <1050269+PiotrZakrzewski@users.noreply.github.com>
2025-06-21 11:18:08 +1000

    Exercise 7

--- ch1/1.jl
+++ ch1/1.jl
@@ -5,3 +5,8 @@
  @variables x
  ex = x^2 + x*3 - 4
  @test Symbolics.substitute(ex, Dict([x => 2])) == 6
+
+ # Example 7
+ @variables p
+ ex = p^2 + 2*p
+ @test Symbolics.substitute(ex, Dict([p => 4])) == 24
